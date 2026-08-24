fbe5ad581c4f869565e646347abf589b0ef756c3
Rohit Ghumare <ghumare64@gmail.com>
2026-08-24 12:56:21 +0100

    fix(console-ui): import the annotation tool type

--- console/web/src/components/ui/Annotations.tsx
+++ console/web/src/components/ui/Annotations.tsx
@@ -24,7 +24,7 @@ import { IconButton } from '@/components/ui/IconButton'
 import { Input } from '@/components/ui/Input'
 import {
   type Annotation,
-  type AnnotationKind,
+  type AnnotationTool,
   annotationKind,
   containedImageBox,
 } from '@/lib/annotations'
